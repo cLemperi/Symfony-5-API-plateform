f2674cc44430b5a40a7b28f328d79014d9f6d314
Charles Lemperiere <lemperiere.charles@gmail.com>
2021-08-12 18:16:24 +0200

    fix

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,11 +5,28 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+//read:collection pour lire une collection d'article
+//read:item correspond à la lecture d'un seul item
+/*ici je souhaite afficher les attributs title et slug quand je demande une collection d'article mais afficher 
+    mais afficher en plus le contennu ainsi que la date quand je fais une requete d'un seul article
+*/
 
 /**
  * @ORM\Entity(repositoryClass=PostRepository::class)
  */
-#[ApiResource()]
+#[ApiResource(
+    normalizationContext:['groups'=> ['read:collection']],
+    itemOperations:[
+        'get' => [
+            'normalization_context' => ['groups'=> ['read:collection', 'read:item', 'read:Post']],
+        'put' => [
+            'normalization_context' => ['groups' => ['put']],
+            ],
+        ],
+    ],
+)]
 class Post
 {
     /**
@@ -17,26 +34,31 @@ class Post
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
+    #[Groups(['read:collection'])]
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
+    #[Groups(['read:collection'])]
     private $title;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
+    #[Groups(['read:collection'])]
     private $slug;
 
     /**
      * @ORM\Column(type="text")
      */
+    #[Groups(['read:item'])]
     private $content;
 
     /**
      * @ORM\Column(type="datetime_immutable", nullable=true)
      */
+    #[Groups(['read:item'])]
     private $uptdatedAt;
 
     /**
